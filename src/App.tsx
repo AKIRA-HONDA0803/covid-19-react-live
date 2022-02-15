7b457ac04e552352322d8d8b2b03ab912802ccdf
AkiraHonda <05410kira@gmail.com>
2022-02-15 17:43:43 +0900

    Chartsコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,78 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Cards from './features/covid/Cards/Cards';
+// import { Chart } from 'chart.js/auto';
+import Charts from './features/covid/Charts/Charts';
+
+
+// import { registerables } from 'chart.js';
+// Chart.register(...registerables);
+
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Cards />
+        <Charts />
       </header>
     </div>
   );
